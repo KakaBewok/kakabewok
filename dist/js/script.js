73930fa1450bed9465fe72e3950daf7baed0ad9b
kkbwk <rizalnov667@gmail.com>
2022-12-12 14:56:59 +0700

    add shortcut to top button

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,23 @@
 // hamburger
-const hamburger = document.querySelector("#humburger");
-const navMenu = document.querySelector("#nav-menu");
+const hamburger = document.querySelector('#humburger');
+const navMenu = document.querySelector('#nav-menu');
+const shortcut = document.querySelector('#shortcut');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("hamburger-active");
-  navMenu.classList.toggle("hidden");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
 });
 
 // navbar fixed
 window.onscroll = function () {
-  const header = document.querySelector("header");
+  const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
 
   if (window.scrollY > fixedNav) {
-    header.classList.add("navbar-fixed");
+    header.classList.add('navbar-fixed');
+    shortcut.classList.remove('hidden');
   } else {
-    header.classList.remove("navbar-fixed");
+    header.classList.remove('navbar-fixed');
+    shortcut.classList.add('hidden');
   }
 };
